0ff06de972f6f7970b044d16c83ab713ed46e940
termokop <termokop564@gmail.com>
2023-03-17 16:20:45 +0400

    created project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    let name = "Yurii";
+
+    let year_priorities = ["Стоїцизм", "Англійська", "Програмування"]
+</script>
+
+<h1> Welcome back, {name} </h1>
+
+<h2>
+    Priorities in the year    
+</h2>
+
+<ol>
+    <li>{year_priorities}</li>    
+</ol>
+
+
+<h2>(17/03/2023)</h2>
+<ol>
+    <li><del>Delete all games</del></li>    
+    <li><del>Clean laptop</del></li>
+    <li><del>Workout</del></li>
+    <li>Find way to pay for hosting</li>
+    <li><del>Cut hair</del></li>
+    <li>Learn Svelte</li>
+    <li>Create +2 lessons for UkrGe</li>
+</ol>
